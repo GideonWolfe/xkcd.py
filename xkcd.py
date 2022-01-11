b92808ab4d8965311e3cc1ab85c63d6741472307
gideonwolfe <wolfeg2@wwu.edu>
2022-01-10 20:44:09 -0800

    better shadow

--- xkcd.py
+++ xkcd.py
@@ -6,13 +6,8 @@ import os
 import random
 import urllib.request
 import subprocess
+import colorsys
 
-
-# Set up command line options
-parser = argparse.ArgumentParser(description='Generate XKCD wallpapers.')
-parser.add_argument('-b', '--bgcolor', default="#EAD494", dest='bgcolor', action='store', help='color value in hex notation')
-parser.add_argument('-n', '--comicnum', default="random", dest='comicnum', action='store', help='which comic to use')
-args = parser.parse_args()
 
 
 # Returns the number of the latest comic
@@ -57,29 +52,54 @@ def download_image(url, cacheDir):
     full_path = cacheDir + "/comic.png"
     urllib.request.urlretrieve(url, full_path)
 
-def add_drop_shadow():
-    source_path = setup_cache_dir() + "/comic.png"
-    target_path = setup_cache_dir() + "/shadowcomic.png"
-    args = ["convert", source_path, "(", "-clone", "0", "-background", "gray", "-shadow", "80x3+10+10", ")", "-reverse", "-background", "none", "-layers", "merge", "+repage", target_path]
-    subprocess.run(args, capture_output=True)
 
-def add_background():
+def add_background(bgcolor):
     source_path = setup_cache_dir() + "/shadowcomic.png"
     target_path = setup_cache_dir() + "/background.png"
-    args = ["convert", source_path, "-gravity", "center", "-background", "blue", "-extent", "1920x1080", target_path]
+    cmdargs = ["convert", source_path, "-gravity", "center", "-background", bgcolor, "-extent", "1920x1080", target_path]
+    subprocess.run(cmdargs, capture_output=True)
+
+def generate_shadow_color(bgcolor):
+    rawHex = bgcolor.lstrip("#")
+    rgb = tuple(int(rawHex[i:i+2], 16) for i in (0, 2, 4))
+    print(rgb)
+    colorList = list(rgb)
+    for i in range(0, len(colorList)):
+        colorList[i] = int(colorList[i]*0.8)
+    rgb = tuple(colorList)
+    #  hsv = colorsys.rgb_to_hsv(*rgb)
+    #  print(hsv)
+    print(rgb)
+    return(rgb)
+
+def add_drop_shadow(bgcolor):
+    shadow_color = list(generate_shadow_color(bgcolor))
+    color_string = "rgb({},{},{})".format(shadow_color[0], shadow_color[1], shadow_color[2])
+    source_path = setup_cache_dir() + "/comic.png"
+    target_path = setup_cache_dir() + "/shadowcomic.png"
+    #  args = ["convert", source_path, "(", "-clone", "0", "-background", "gray", "-shadow", "80x3+10+10", ")", "-reverse", "-background", "none", "-layers", "merge", "+repage", target_path]
+    args = ["convert", source_path, "(", "-clone", "0", "-background", color_string, "-shadow", "80x3+10+10", ")", "-reverse", "-background", "none", "-layers", "merge", "+repage", target_path]
     subprocess.run(args, capture_output=True)
 
 
 def main():
     # Setup
-    #  setup_cache_dir()
+    # Set up command line options
+    parser = argparse.ArgumentParser(description='Generate XKCD wallpapers.')
+    parser.add_argument('-b', '--bgcolor', default="#EAD494", dest='bgcolor', action='store', help='color value in hex notation')
+    parser.add_argument('-n', '--comicnum', default="random", dest='comicnum', action='store', help='which comic to use')
+    args = parser.parse_args()
 
-    #  url = get_random_comic()
+    setup_cache_dir()
 
-    #  download_image(url, setup_cache_dir())
+    # Get a random comic URL
+    url = get_random_comic()
 
-    #  add_drop_shadow()
-    add_background()
+    # Grab the image
+    download_image(url, setup_cache_dir())
+
+    add_drop_shadow(args.bgcolor)
+    add_background(args.bgcolor)
 
 
 
